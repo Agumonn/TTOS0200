b369c50aec21e7d74bd61c56e72ed15150cdee22
K8522 <K8522@student.jamk.fi>
2017-09-06 09:04:55 +0300

    VK01 added and namespace changed

--- OOdemot/Program.cs
+++ OOdemot/Program.cs
@@ -1,7 +1,9 @@
-﻿using JAMK.IT; //helpottaa meidän tekemien luokkien löytämistä
+﻿//TTOS0200 olio-ohjelmointi viikkotehtävät
+//Aku Karttunen K8522
+using JAMK.IT; //helpottaa meidän tekemien luokkien löytämistä
 using System;
 
-namespace OOdemot
+namespace JAMK.IT
 {
   class Program
   {

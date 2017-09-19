902c31a6d92c7a895d939303cc8a7a934546b79c
Aku Karttunen <K8522@student.jamk.fi>
2017-09-19 23:54:18 +0300

    tasks 1-5 done from week 38

---
+++ TEHTAVAT2/Pesukone.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEHTAVAT2
+{
+    class Pesukone
+    {
+        public bool virta;
+        public bool hana;
+        public bool paalle;
+        public int ohjelma;
+        public int linkous;
+    }
+    class toiminto
+    {
+        public static void pese()
+
+        {
+
+            Pesukone pese  = new Pesukone();
+            int luku = 1;
+            int valinta = new int();
+
+            while (luku == 1)
+            {
+                Console.WriteLine("PESUKONE");
+                Console.WriteLine("Paina 1 Laittaaksesi virrat päälle ");
+                Console.WriteLine("Paina 2 avataksesi hanan");
+                Console.WriteLine("Paina 3 valitaksesi ohjelman");
+                Console.WriteLine("Paina 4 valitaksesi linkouksen");
+                Console.WriteLine("Paina 5 käynnistääksesi ohjelman");
+                Console.WriteLine("Paina 0 lopettaaksesi");
+                valinta = int.Parse(Console.ReadLine());
+
+                switch (valinta)
+                {
+
+                    case 0:
+                        luku = 0;
+                        break;
+
+                    case 1:
+                        if (pese.virta == false)
+                        {
+                            pese.virta = true;
+                            Console.WriteLine("Virta päällä: {0}", pese.virta);
+                        }
+                        else if (pese.virta == true)
+                        {
+                            pese.virta = false;
+                            Console.WriteLine("Virta päällä: {0}", pese.virta);
+                        }
+                        break;
+                    case 2:
+                        if (pese.hana == false)
+                        {
+                            pese.hana = true;
+                            Console.WriteLine("Hana auki: {0}", pese.hana);
+                        }
+                        else if (pese.hana == true)
+                        {
+                            pese.hana = false;
+                            Console.WriteLine("Hana auki: {0}", pese.hana);
+                        }
+                        break;
+
+                    case 3:
+                        if (pese.virta == true)
+                        {
+                            Console.WriteLine("Syötä haluttu ohjelmanro: ");
+                            pese.ohjelma = int.Parse(Console.ReadLine());
+                            Console.WriteLine("pesukone päällä: {0}", pese.virta);
+                            Console.WriteLine("Hana auki: {0}", pese.hana);
+                            Console.WriteLine("ohjelma nro: {0} valittu", pese.ohjelma);
+                            Console.WriteLine("linkous {0} valittu", pese.linkous);
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("pesukoneen pitää olla ensin päällä!");
+                        }
+                        break;
+                    case 4:
+                        if (pese.virta == true)
+                        {
+                            Console.WriteLine("Syötä haluttu linkous: ");
+                            pese.linkous = int.Parse(Console.ReadLine());
+                            Console.WriteLine("pesukone päällä: {0}", pese.virta);
+                            Console.WriteLine("Hana auki: {0}", pese.hana);
+                            Console.WriteLine("ohjelma nro: {0} valittu", pese.ohjelma);
+                            Console.WriteLine("linkous {0} valittu", pese.linkous);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("pesukoneen pitää olla ensin päällä!");
+                        }
+                        break;
+                    case 5:
+                        if (pese.virta == true && pese.hana==true)
+                        {
+                            pese.paalle = true;
+                            Console.WriteLine("pyykit peseytyy: {0} ",pese.paalle);
+                            Console.WriteLine(" ");
+                            Console.WriteLine(" ");
+                            Console.WriteLine("pyykit pesty ");
+                            pese.paalle = false;
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("pesukoneen pitää olla ensin päällä ja hanan pitää olla auki");
+                        }
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ TEHTAVAT2/Vehicle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEHTAVAT2
+{
+    class Vehicle
+    {
+        public string name;
+        public int speed;
+        public int tyres;
+        public void PrintData()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(speed);
+            Console.WriteLine(tyres);
+
+        }
+        public string ToString()
+        {
+
+            return (String.Format("{0} {1} {2}", name, speed, tyres));
+
+        }
+    }
+
+    class tiedot
+    {
+        public static void show()
+        {
+            Vehicle make = new Vehicle();
+            int luku = 1;
+            int valinta = new int();
+
+            while (luku == 1)
+            {
+                Console.WriteLine("VEHICLE");
+                Console.WriteLine("Paina 1 lisätäksesi uusi vehicle ");
+                Console.WriteLine("Paina 2 näyttääksesi olevat vehiclet");
+                Console.WriteLine("Paina 3 näytä merkkijono");
+                Console.WriteLine("Paina 0 lopettaaksesi");
+                valinta = int.Parse(Console.ReadLine());
+
+                switch (valinta)
+                {
+                    case 0:
+                        luku = 0;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("anna vehiclelle nimi ");
+                        make.name = Console.ReadLine();
+                        Console.WriteLine("anna vehiclelle nopeus ");
+                        make.speed = int.Parse(Console.ReadLine());
+                        Console.WriteLine("anna vehiclelle rengaskoko ");
+                        make.tyres = int.Parse(Console.ReadLine());
+                        make.ToString();
+
+                        break;
+                    case 2:
+                        make.PrintData();
+                        break;
+                    case 3:
+                        Console.WriteLine(make.ToString());
+                        break;
+
+                }
+            }
+        }
+    }
+}

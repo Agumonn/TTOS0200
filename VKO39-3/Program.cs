52bd07875564afaa7e1b00c2f22c4945193cdd95
Aku Karttunen <K8522@student.jamk.fi>
2017-09-26 20:24:26 +0300

    Task 3 added

---
+++ VKO39-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKO39_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vahvistin vahvistin = new Vahvistin();
+
+            do
+            {
+
+                Console.WriteLine("Give a new volume value (0-100) > ");
+                string sisaantulo = Console.ReadLine();
+                int luku;
+                bool valinta = int.TryParse(sisaantulo, out luku);
+
+                if (valinta)
+                {
+                    
+                    vahvistin.Volume = luku;
+                }
+                else
+                {
+                    break;
+                }
+
+
+                Console.WriteLine(" -> Amplifier volume is set to : {0}", vahvistin.Volume);
+
+            }
+            while (true);
+        }
+    }
+}
---
+++ VKO39-3/Vahvistin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Tehtävänäsi on ohjelmoida yksinkertaisen vahvistimen toiminta, jolla voidaan kontrolloida äänenvoimakkuutta välillä 0-100. 
+//Toteuta Vahvistin-luokka ja tee pääohjelma, jolla luot olion Vahvistin-luokasta. Säädä ja kokeile vahvistinta eri äänenvoimakkuuksilla. 
+//Käytä Vahvistin-luokassa get- ja set-aksessoreita. get-aksessori palauttaa äänenvoimakkuuden ja set-aksessori rajaa äänenvoimakkuuden arvoa.
+namespace VKO39_3
+{
+    class Vahvistin
+    {
+        private const int maxvolume = 100;
+        private const int minvolume = 0;
+        private int volume;
+        public int Volume
+        {
+
+            get
+            {
+                return volume;
+            }
+
+            set
+            {
+                if (value > maxvolume)
+                {
+                    volume = maxvolume;
+                    Console.WriteLine("-> Too much volume -  Amplifier volume is set to maximum : {0}", volume);
+                }
+                else if (value < minvolume)
+                {
+                    volume = minvolume;
+                    Console.WriteLine("-> Too low volume - Amplifier volume is set to minimum : {0}", volume);
+                }
+                else
+                    volume = value;
+            }
+
+        }
+    }
+}

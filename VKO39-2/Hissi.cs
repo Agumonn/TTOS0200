1ea7981d8b372319a09fe14fff8bb5c1755e0308
Aku Karttunen <K8522@student.jamk.fi>
2017-09-26 20:15:22 +0300

    Added task 2

---
+++ VKO39-2/Hissi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKO39_2
+{
+    class Hissi
+    {
+
+
+        private const int maxkerros = 5;
+        private const int minkerros = 1;
+        private int kerros;
+        public int Kerros
+        {
+
+            get
+            {
+                return kerros;
+            }
+
+            set
+            {
+                if (value > maxkerros)
+                { 
+                    kerros = maxkerros;
+                    Console.WriteLine("Ylin kerros on {0}", kerros);
+                }
+                else if (value < minkerros)
+                {
+                    kerros = minkerros;
+                    Console.WriteLine("Alin kerros on {0}", kerros);
+                }
+                else
+                    kerros = value;
+            }
+
+        }
+    }
+}
---
+++ VKO39-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKO39_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hissi hissi = new Hissi();
+
+            do
+            {
+
+                Console.WriteLine("Syötä kerros.");
+                string sisaantulo = Console.ReadLine();
+                int luku;
+                bool valinta = int.TryParse(sisaantulo, out luku);
+
+                if (valinta)
+                {
+                    hissi.Kerros = luku;
+                }
+                else
+                {
+                    break;
+                }
+
+
+                Console.WriteLine("{0} kerros ", hissi.Kerros);
+
+            }
+            while (true);
+        }
+    }
+}

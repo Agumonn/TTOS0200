8b8b3596e22fb5a827e13d8d4abf9615577c5bf2
Aku Karttunen <K8522@student.jamk.fi>
2017-10-03 20:14:21 +0300

    Added task 8

---
+++ VKO40-5/Eläin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKO40_5
+{
+    public class Eläin
+    {
+        public string Nimi { get; set; }
+        public int Ikä { get; set; }
+        public string Sukupuoli { get; set; }
+        public string Rotu { get; set; }
+        public string Väri { get; set; }
+        public int Paino { get; set; }
+        public string Ääni { get; set; }
+
+        public override string ToString()
+        {
+            return " Nimi: " + Nimi + " Ikä: " + Ikä + " Sukupuoli: " + Sukupuoli + " Rotu: " + Rotu + " Väri: " + Väri + " Paino: " + Paino + " Ääni: " + Ääni;
+        }
+
+
+    }
+    public class Kissa :Eläin
+    {
+        public int Hännänpituus { get; set; }
+        public int Tassunkoko { get; set; }
+        public override string ToString()
+        {
+            return " Hännänpituus: " + Hännänpituus + " Tassunkoko: " + Tassunkoko;
+        }
+    }
+    public class Koira : Eläin
+    {
+        public int Hännänpituus { get; set; }
+        public int Tassunkoko { get; set; }
+        public override string ToString()
+        {
+            return " Hännänpituus: " + Hännänpituus + " Tassunkoko: " + Tassunkoko;
+        }
+
+
+    }
+    public class Elefantti : Eläin
+    {
+        public int Kärsänpituus { get; set; }
+        public int Hännänpituus { get; set; }
+        public override string ToString()
+        {
+            return " Kärsänpituus: " + Kärsänpituus + " Hännänpituus: " + Hännänpituus;
+        }
+
+
+    }
+
+}
--- VKO40-5/Program.cs
+++ VKO40-5/Program.cs
@@ -10,6 +10,47 @@ namespace VKO40_5
     {
         static void Main(string[] args)
         {
+            Kissa kisu = new Kissa();
+            Koira doge = new Koira();
+            Elefantti möhkö = new Elefantti();
+
+
+            kisu.Nimi = "kisu";
+            kisu.Ikä = 11;
+            kisu.Paino = 20;
+            kisu.Rotu = "maatiaiskissa";
+            kisu.Hännänpituus = 30;
+            kisu.Sukupuoli = "naaras";
+            kisu.Tassunkoko = 14;
+            kisu.Väri = "musta";
+            kisu.Ääni = "Miau";
+
+
+            doge.Nimi = "BENI";
+            doge.Ikä = 11;
+            doge.Paino = 50;
+            doge.Rotu = "BÉRNARDI";
+            doge.Hännänpituus = 30;
+            doge.Sukupuoli = "boiii";
+            doge.Tassunkoko = 14;
+            doge.Väri = "musta";
+            doge.Ääni = "vuhvuhv";
+
+            möhkö.Nimi = "Möhköfantti";
+            möhkö.Ikä = 20;
+            möhkö.Paino = 2000;
+            möhkö.Rotu = "AFrikan norsu";
+            möhkö.Hännänpituus = 30;
+            möhkö.Sukupuoli = "naaras";
+            möhkö.Kärsänpituus = 14;
+            möhkö.Väri = "harmaa";
+            möhkö.Ääni = "tuut";
+
+            Console.WriteLine(kisu.ToString());
+            Console.WriteLine(doge.ToString());
+            Console.WriteLine(möhkö.ToString());
+
+
         }
     }
 }

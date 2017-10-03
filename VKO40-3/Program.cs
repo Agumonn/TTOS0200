9c5fd1db2c9fdcbad463a7ece0883ce4f2974bfa
Aku Karttunen <K8522@student.jamk.fi>
2017-10-03 19:34:52 +0300

    Task 6 done

--- VKO40-3/Program.cs
+++ VKO40-3/Program.cs
@@ -10,6 +10,44 @@ namespace VKO40_3
     {
         static void Main(string[] args)
         {
+
+            Radio matkaradio = new Radio();
+
+
+            {
+                Console.WriteLine("Tervetuloa radioon.");
+                Console.WriteLine("Paina 1 kytkeäksesi radion päälle");
+
+                int luku = int.Parse(Console.ReadLine());
+                if (luku == 1)
+                {
+                    matkaradio.Power = true;
+                    Console.WriteLine("Radio päällä");
+
+                }
+                else
+                {
+                    Console.WriteLine("Kytke radio päälle painamalla 1");
+                }
+
+                if (matkaradio.Power == true)
+                {
+
+                    Console.WriteLine("Syötä taajuus (2KHz-26KHz) ");
+                    matkaradio.Frequency = int.Parse(Console.ReadLine());
+
+                }
+                if (matkaradio.Power == true)
+                {
+                    Console.WriteLine("Syötä Äänenvoimakkuus (0-9) ");
+
+                    matkaradio.Volume = int.Parse(Console.ReadLine());
+
+                }
+                Console.WriteLine(matkaradio.ToString());
+
+
+            }
         }
     }
 }
--- VKO40-3/Radio.cs
+++ VKO40-3/Radio.cs
@@ -9,8 +9,47 @@ namespace VKO40_3
     public class Radio
     {
         public bool Power { get; set; }
-        public int Volume { get; set; }
-        public int Frequency { get; set; }
+        private const int Maxfrequency = 26000;
+        private const int Minfrequency = 2000;
+        private const int MaxVolume = 9;
+        private const int MinVolume = 0;
+        private int frequency;
+        private int volume;
+
+
+        public int Frequency
+        {
+            get { return frequency; }
+            set
+            {
+                if (value > Maxfrequency)
+                    frequency = Maxfrequency;
+                else if (value < Minfrequency)
+                    frequency = Minfrequency;
+                else
+                    frequency = value;
+            }
+        }
+
+        public int Volume {
+            get { return volume; }
+            set
+            {
+                if (value > MaxVolume )
+                    volume = MaxVolume;
+                else if (value < MinVolume)
+                    volume = MinVolume;
+                else
+                    volume = value;
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format(" Radio päällä: {0}\n Taajuus: {1}\n Äänenvoimakkuus: {2}\n", Power, frequency, volume);
+        }
+
+
+
 
     }
 }

7f7eabc7cfefb9d69480ea4e76ae42d157c28f0d
K8522 <K8522@student.jamk.fi>
2017-10-03 16:51:44 +0300

    Continued task 7 a.k.a bookshelf

--- VKO40-4/Bookshelf.cs
+++ VKO40-4/Bookshelf.cs
@@ -6,7 +6,99 @@ using System.Threading.Tasks;
 
 namespace VKO40_4
 {
-    class Bookshelf
-    {
-    }
+    public class Bookshelf
+        {
+        public string Name;
+        
+        public override string ToString()
+            {
+                return "Nimi: " + Name;
+            }
+   
+        }
+    public class Book :Bookshelf
+        {
+            public string Author {get;set;}
+            public string Publisher {get;set;}
+            public int Pagecount {get;set;}
+        public override string ToString()
+            {
+                return " kirjailija: " + Author + " Julkaisija: " + Publisher + " Sivujen lkm:" + Pagecount ;
+            }
+
+        }
+    public class Magazine :Bookshelf
+        {
+        public string Publisher {get;set;}
+        public int Pagecount {get;set;}
+        public string Issuenumber {get; set;}
+
+        public override string ToString()
+            {
+                return " Julkaisija: " + Publisher + " Sivujen lkm:" + Pagecount + "numero"+ Issuenumber;
+            }
+
+        }
+    public class Video :Bookshelf
+        {
+        public string Director {get;set;}
+        public string Writer {get;set;}
+        public string Leadactor {get;set;}
+        public string Leadactress {get;set;}
+        public int Year {get;set;}
+
+        public override string ToString()
+            {
+                return " Ohjaaja: " + Director + " käsikirjoittaja: " + Writer + " näyttelijät:" + Leadactor + " " + Leadactress+ " vuosi:" + Year ;
+            }
+
+        }
+    public class Bluray :Video
+        {
+        public string Format{get;set;}
+        public override string ToString()
+            {
+                return "Format: " + Format;
+            }
+
+        }
+    public class Dvd :Video
+        {
+        public string Format{get;set;}
+        public override string ToString()
+            {
+                return "Format: " + Format;
+            }
+
+        }
+    public class Music :Bookshelf
+        { 
+        public string Artist{get;set;}
+        public int Trackcount{get;set;}
+        public override string ToString()
+            {
+                return "Artisti: " + Artist + " Biisien lkm "+ Trackcount ;
+            }
+
+        }
+    public class Vinyl : Music
+        {
+        public string Format;
+        public override string ToString()
+            {
+                return "Format: " + Format;
+            }
+        }
+    public class Cd : Music
+        {
+        public string Format;
+        public override string ToString()
+            {
+                return "Format: " + Format;
+            }
+        }
+
+
+
+
 }
--- VKO40-4/Program.cs
+++ VKO40-4/Program.cs
@@ -10,6 +10,64 @@ namespace VKO40_4
     {
         static void Main(string[] args)
         {
+            Cd levy = new Cd();
+            Vinyl lätty = new Vinyl();
+            Dvd leffa = new Dvd();
+            Bluray lefu = new Bluray();
+            Magazine lehti = new Magazine();
+            Book kirja = new Book();
+
+            levy.Name = "Mellon collie and infinite sadness";
+            levy.Artist = "The Smashing pumpkins";
+            levy.Trackcount= 11;
+            levy.Format="cd";
+        
+
+            lätty.Name ="Power corruption and lies";
+            lätty.Artist="new order";
+            lätty.Trackcount=10;
+            lätty.Format="vinyl";
+
+            leffa.Name ="Blade runner";
+            leffa.Director="Ridley scott";
+            leffa.Writer="unknown";
+            leffa.Year= 1982;
+            leffa.Leadactor="harrison ford";
+            leffa.Leadactress="unknown";
+            leffa.Format="dvd";
+            
+
+            lefu.Name ="gladiator";
+            lefu.Director="Ridley scott";
+            lefu.Writer="unknown";
+            lefu.Year=2001;
+            lefu.Leadactor="unknown";
+            lefu.Leadactress="unknown";
+            lefu.Format="blu-ray";
+
+            lehti.Name="iltalehti";
+            lehti.Pagecount= 55;
+            lehti.Publisher="sanoma";
+
+            kirja.Name="Harry potter";
+            kirja.Author="J.K. Rowling";
+            kirja.Publisher="otava";
+            kirja.Pagecount=455;
+
+
+            Console.WriteLine(kirja.ToString());
+            Console.WriteLine(lehti.ToString());
+            Console.WriteLine(leffa.ToString());
+            Console.WriteLine(lefu.ToString());
+            Console.WriteLine(lätty.ToString());
+            Console.WriteLine(levy.ToString());
+
+
+
+
+
+
+
         }
     }
 }

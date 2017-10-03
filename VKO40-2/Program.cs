babbe0a41b244b6a7051d4320f9cc53d86b23eea
K8522 <K8522@student.jamk.fi>
2017-10-03 12:15:09 +0300

    Added TASK 5

---
+++ VKO40-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKO40_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vene Yamaha = new Vene();
+            Polkupyörä Shimano = new Polkupyörä();
+
+            Yamaha.Nimi = "Yamaha";
+            Yamaha.Malli = "SUperBoat";
+            Yamaha.Vuosimalli = 2010;
+            Yamaha.Väri = "Pinkki";
+            Yamaha.SeatCount = 10;
+            Yamaha.BoatType = "moottorivene";
+
+            Shimano.Nimi = "SHIMANO";
+            Shimano.Malli = " XX";
+            Shimano.Vuosimalli = 2010;
+            Shimano.Väri = "Sininen ";
+            Shimano.Vaihde = false;
+            Shimano.Vaihdemalli = "";
+
+            Console.WriteLine(Yamaha.ToString());
+            Console.WriteLine(Shimano.ToString());
+        }
+    }
+}
---
+++ VKO40-2/Vehicle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKO40_2
+{
+
+        public class Vehicle
+        {
+            public string Nimi { get; set; }
+            public string Malli { get; set; }
+            public string Väri { get; set; }
+            public int Vuosimalli { get; set; }
+
+
+        public override string ToString()
+            {
+                return "Nimi: " + Nimi + " Malli: " + Malli + " Väri: " + Väri + " Vuosimalli: " + Vuosimalli + " ";
+            }
+
+
+        }
+        public class Polkupyörä : Vehicle
+        {
+            public bool Vaihde { get; set; }
+
+            public string Vaihdemalli { get; set; }
+
+            public override string ToString()
+            {
+                return base.ToString() + "Vaihteet: " + Vaihde + " Vaihdemalli: " + Vaihdemalli + "\n";
+            }
+
+        }
+        public class Vene : Vehicle
+        {
+            public int SeatCount { get; set; }
+
+            public string BoatType { get; set; }
+
+            public override string ToString()
+            {
+                return base.ToString() + "Penkkien lkm: " + SeatCount + " Venetyyppi: " + BoatType + "\n";
+            }
+
+        }
+
+}

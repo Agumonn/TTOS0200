cb91a2d22bb7f79412dda0a2342d6d2da55ef192
k8522 <k8522@ELEK-436-WS-19>
2017-10-25 10:49:55 +0300

    Task 1 for week 44

---
+++ VKO44-1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKO44_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KÄYTTÄKÄÄ AINA TRY-CATCHIA
+            try
+            {
+                string nimi;
+                string filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string file = filepath + @"\testi.txt";
+                //kirjoitetaan käyttäjän antamat rivit tiedostoon
+                //luodaan StreamWriter tyyppinen olio johon kirjoitetaan
+                StreamWriter sw = new StreamWriter(file);
+                do
+                {
+                    Console.Write("Anna nimi (Tyhjä rivi lopettaa kirjoittamisen):");
+                    nimi = Console.ReadLine();
+                    if (nimi.Length > 0)
+                    {
+                        sw.WriteLine(nimi);
+                    }
+                } while (nimi.Length != 0);
+                sw.Close();
+                //avataan tiedosto uudestaan ja luetaan sisältö
+                if (File.Exists(file))
+                {
+                    string teksti = File.ReadAllText(file);
+                    Console.WriteLine(teksti);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+    }
+}

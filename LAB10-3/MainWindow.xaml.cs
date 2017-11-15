1670137a49289fa55d37ff10cae448f05fef837d
k8522 <k8522@ELEK-436-WS-19>
2017-11-15 11:04:13 +0200

    Tuntitehtävä

--- LAB10-3/MainWindow.xaml.cs
+++ LAB10-3/MainWindow.xaml.cs
@@ -34,13 +34,39 @@ namespace LAB10_3
             int ikkunapa = korkeus * leveys;
             int karmipiiri = 2 * korkeus + 2 * leveys;
             int lasinpa = (korkeus - (2 * karmi)) * (leveys - (2 * karmi));
+
+            Ikkuna.MaxHeight = 300;
+            Ikkuna.MinHeight = 30;
+
+            if (korkeus > Ikkuna.MaxHeight)
+            {
+                tulosbox.Text = "Liian suuri korkeus";
+            }
+            if (leveys > Ikkuna.MaxHeight)
+            {
+                tulosbox.Text = "Liian suuri leveys";
+            }
+            if (korkeus > Ikkuna.MinHeight)
+            {
+                tulosbox.Text = "Liian pieni korkeus";
+            }
+            if (leveys > Ikkuna.MinHeight)
+            {
+                tulosbox.Text = "Liian pieni leveys";
+            }
+
             ikkunapa = ikkunapa / 10;
             lasinpa = lasinpa / 10;
+            karmipiiri = karmipiiri / 10;
+
+            Ikkuna.Height = korkeus / 10;
+            Ikkuna.Width = leveys / 10;
+            Ikkuna.StrokeThickness = karmi / 10;
 
             
             ikkunapabox.Text = ikkunapa.ToString() + " cm^2";
             lasinpabox.Text = lasinpa.ToString() + " cm^2" ;
-            karmipiiribox.Text = karmipiiri.ToString();
+            karmipiiribox.Text = karmipiiri.ToString() + " cm";
         }
     }
 }
